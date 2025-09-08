bd59fa80cb1951f0231814f3dce9ed7a2727bf3a
Jonathan Gil <jhongilp@gmail.com>
2025-09-08 17:26:13 -0500

    add initial setup to stablish ws connection

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github/jhongilp/websockets-go
 
 go 1.25.0
+
+require github.com/gorilla/websocket v1.5.3 // indirect
--- main.go
+++ main.go
@@ -12,5 +12,7 @@ func main() {
 }
 
 func setupAPI() {
+	manager := NewManager()
 	http.Handle("/", http.FileServer(http.Dir("./frontend")))
+	http.HandleFunc("/ws", manager.ServeWS)
 }
