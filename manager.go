47d0f201e01e5fafc784da25c461fa1c54c34988
Jonathan Gil <jhongilp@gmail.com>
2025-09-10 15:07:21 -0500

    add CheckOrigin

--- manager.go
+++ manager.go
@@ -12,6 +12,7 @@ import (
 
 var (
 	websocketUpgrader = websocket.Upgrader{
+		CheckOrigin:     checkOrigin,
 		ReadBufferSize:  1024,
 		WriteBufferSize: 1024,
 	}
@@ -91,4 +92,9 @@ func (m *Manager) removeClient(client *Client) {
 		client.connection.Close()
 		delete(m.clients, client)
 	}
+}
+
+func checkOrigin(r *http.Request) bool {
+	origin := r.Header.Get("Origin")
+	return origin == "http://localhost:8080"
 }
